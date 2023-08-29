134f172c6d92bf20695873c602fe09ccc87e18d6
Engine <rg.dartigue@gmail.com>
2023-08-29 13:03:15 -0400

    Began work in install.js file. Used vectorPrompt to store the beforeinstallprompt event. (Really wish I could have named my app VectorPrompt at the time of creation because it sounds cooler.)

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,8 +1,15 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let vectorPrompt;
+
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+// DONE
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    vectorPrompt = event;
+    butInstall.style.display= 'block';
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {});
